0b7a292b1a266a916d15118850f6c70b500d9d43
Sayori-h <3186385448@qq.com>
2023-07-01 09:27:52 +0800

    p3d实时数据导出的首次提交

---
+++ DataSend/DataSend.cpp
@@ -0,0 +1,177 @@
+﻿//// Copyright (c) 2010-2018 Lockheed Martin Corporation. All rights reserved.
+//// Use of this file is bound by the PREPAR3D® SOFTWARE DEVELOPER KIT END USER LICENSE AGREEMENT
+////------------------------------------------------------------------------------
+////
+////  SimConnect Set Data Sample
+//// 
+////	Description:
+////				When Ctrl-Shift-A is pressed, the user aircraft is moved
+////				to a new location. When Ctrl+Shift+B is pressed, landing
+////              lights are turned on, AMBIENT WIND VELOCITY are increased, and
+////              the main exit is opened halfway.
+////------------------------------------------------------------------------------
+//
+//#include <windows.h>
+//#include <tchar.h>
+//#include <stdio.h>
+//#include "SimConnect.h"
+//#include <strsafe.h>
+//
+//int     quit = 0;
+//HANDLE  hSimConnect = NULL;
+//
+//enum GROUP_ID {
+//	GROUP_INIT,
+//	GROUP_STRUCT,
+//};
+//
+//enum INPUT_ID {
+//	INPUT_INIT,
+//	INPUT_STRUCT,
+//};
+//
+//enum EVENT_ID {
+//	EVENT_SIM_START,
+//	EVENT_INIT,
+//	EVENT_STRUCT,
+//};
+//
+//enum DATA_DEFINE_ID {
+//	DEFINITION_INIT,
+//	DEFINITION_STRUCT,
+//};
+//
+//// Custom struct used to demonstrate setting different data types.
+//
+////struct CustomStruct
+////{
+////	BOOL    LandingLight;   // SIMCONNECT_DATATYPE_INT32
+////	float   Health;         // SIMCONNECT_DATATYPE_FLOAT32
+////	double  Exit;           // SIMCONNECT_DATATYPE_FLOAT64
+////};
+//struct CustomStruct
+//{
+//	BOOL    LandingLight;   // SIMCONNECT_DATATYPE_INT32
+//	double  dWindVelocity;         // SIMCONNECT_DATATYPE_FLOAT32
+//	double  dWindDirection;           // SIMCONNECT_DATATYPE_FLOAT64
+//};
+//
+//
+//
+//void CALLBACK MyDispatchProcSD(SIMCONNECT_RECV* pData, DWORD cbData, void *pContext)
+//{
+//	HRESULT hr;
+//
+//	switch (pData->dwID)
+//	{
+//	case SIMCONNECT_RECV_ID_EVENT:
+//	{
+//		SIMCONNECT_RECV_EVENT *evt = (SIMCONNECT_RECV_EVENT*)pData;
+//
+//		switch (evt->uEventID)
+//		{
+//		case EVENT_SIM_START:
+//		{
+//			// Turn the input events on now
+//			hr = SimConnect_SetInputGroupState(hSimConnect, INPUT_INIT, SIMCONNECT_STATE_ON);
+//			hr = SimConnect_SetInputGroupState(hSimConnect, INPUT_STRUCT, SIMCONNECT_STATE_ON);
+//		}
+//		break;
+//
+//		case EVENT_INIT:
+//		{
+//			SIMCONNECT_DATA_INITPOSITION Init;
+//			Init.Altitude = 5000.0;
+//			Init.Latitude = 47.64210;
+//			Init.Longitude = -122.13010;
+//			Init.Pitch = 0.0;
+//			Init.Bank = -1.0;
+//			Init.Heading = 180.0;
+//			Init.OnGround = 0;
+//			Init.Airspeed = 60;
+//			hr = SimConnect_SetDataOnSimObject(hSimConnect, DEFINITION_INIT, SIMCONNECT_OBJECT_ID_USER, 0, 0, sizeof(Init), &Init);
+//
+//			printf("\nEVENT_INIT received and data sent");
+//		}
+//		break;
+//
+//		case EVENT_STRUCT:
+//		{
+//			CustomStruct Struct;
+//			Struct.LandingLight = 1;
+//			Struct.dWindVelocity = 100.0f;
+//			Struct.dWindDirection = 270;
+//			hr = SimConnect_SetDataOnSimObject(hSimConnect, DEFINITION_STRUCT, SIMCONNECT_OBJECT_ID_USER, 0, 0, sizeof(Struct), &Struct);
+//
+//			printf("\nEVENT_STRUCT received and data sent");
+//		}
+//		break;
+//
+//		default:
+//			break;
+//		}
+//		break;
+//	}
+//
+//	case SIMCONNECT_RECV_ID_QUIT:
+//	{
+//		quit = 1;
+//		break;
+//	}
+//
+//	default:
+//		printf("\nReceived:%d", pData->dwID);
+//		break;
+//	}
+//}
+//
+//void testDataSet()
+//{
+//	HRESULT hr;
+//
+//	if (SUCCEEDED(SimConnect_Open(&hSimConnect, "Set Data", NULL, 0, 0, 0)))
+//	{
+//		printf("\nConnected to Prepar3D!");
+//
+//		// Set up a data definition for positioning data
+//		hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_INIT, "INITIAL POSITION", NULL, SIMCONNECT_DATATYPE_INITPOSITION);
+//
+//		// Set up data definition for a custom struct with different data types
+//		hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_STRUCT, "LIGHT LANDING", NULL, SIMCONNECT_DATATYPE_INT32);
+//		hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_STRUCT, "AMBIENT WIND VELOCITY", NULL, SIMCONNECT_DATATYPE_FLOAT64);
+//		hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_STRUCT, "AMBIENT WIND DIRECTION", NULL, SIMCONNECT_DATATYPE_FLOAT64);
+//
+//		// Request a simulation start event
+//		hr = SimConnect_SubscribeToSystemEvent(hSimConnect, EVENT_SIM_START, "SimStart");
+//
+//		// Create custom events
+//		hr = SimConnect_MapClientEventToSimEvent(hSimConnect, EVENT_INIT, "My.CTRLSHIFTA");
+//		hr = SimConnect_MapClientEventToSimEvent(hSimConnect, EVENT_STRUCT, "My.CTRLSHIFTB");
+//
+//		// Link the custom event to some keyboard keys, and turn the input event off
+//		hr = SimConnect_MapInputEventToClientEvent(hSimConnect, INPUT_INIT, "Ctrl+Shift+A", EVENT_INIT);
+//		hr = SimConnect_SetInputGroupState(hSimConnect, INPUT_INIT, SIMCONNECT_STATE_OFF);
+//		hr = SimConnect_MapInputEventToClientEvent(hSimConnect, INPUT_STRUCT, "Ctrl+Shift+B", EVENT_STRUCT);
+//		hr = SimConnect_SetInputGroupState(hSimConnect, INPUT_STRUCT, SIMCONNECT_STATE_OFF);
+//
+//		// Sign up for notifications for EVENT_INIT and EVENT_STRUCT
+//		hr = SimConnect_AddClientEventToNotificationGroup(hSimConnect, GROUP_INIT, EVENT_INIT);
+//		hr = SimConnect_AddClientEventToNotificationGroup(hSimConnect, GROUP_STRUCT, EVENT_STRUCT);
+//
+//		while (0 == quit)
+//		{
+//			SimConnect_CallDispatch(hSimConnect, MyDispatchProcSD, NULL);
+//			Sleep(1);
+//		}
+//
+//		hr = SimConnect_Close(hSimConnect);
+//	}
+//}
+//
+//int __cdecl _tmain(int argc, _TCHAR* argv[])
+//{
+//
+//	testDataSet();
+//
+//	return 0;
+//}
---
+++ DataSend/cin_data.cpp
@@ -0,0 +1,357 @@
+﻿//Copyright (c) Lockheed Martin Corporation.  All rights reserved.
+//------------------------------------------------------------------------------
+//
+//  SimConnect Camera System Sample
+// 
+//  Description:
+//              Press the 'E' key to create a camera through SimConnect.
+//              After the camera is created use the 'J', 'K', ',', and '.'
+//              keys to modify the camera.
+//
+//              'J' Undocks the view
+//              'K' Docks the view
+//              ',' Changes the view's position
+//              '.' Changes the view's size
+//
+//              These controls are also printed to the console when the sample
+//              application starts.
+//
+//------------------------------------------------------------------------------
+
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include "SimConnect.h"
+#include <strsafe.h>
+
+// SimConnect globals
+static bool G_QUIT = false;
+static HANDLE hSimConnect = NULL;
+static const int SIMCONNECT_SLEEP_TIME = 1;
+
+// Sample project globals
+static const char* PROJECT_NAME = "Camera System";
+static const char* CONTROLS = "\n\n\nCamera System Sample\n\nControls:\n'E' - Create Camera\n'J' - Undock view \n'K' - Dock view \n',' - Toggle view position \n'.' - Toggle view size\n";
+static const char* CAMERA_NAME = "SimconnectCamera";
+
+// Used to change window position and size values
+static const int X1 = 200;
+static const int Y1 = 200;
+static const int X2 = 50;
+static const int Y2 = 50;
+static const int W1 = 512;
+static const int H1 = 256;
+static const int W2 = 256;
+static const int H2 = 512;
+
+// Keep track of current states
+static bool WINDOW_POS_TOGGLE = true;
+static bool WINDOW_SIZE_TOGGLE = true;
+static bool CAMERA_CREATED = false;
+
+static enum GROUP_ID
+{
+	GROUP0
+};
+
+static enum EVENT_ID
+{
+	EVENT_CAMERA_CREATE,
+	EVENT_CAMERA_UNDOCK,
+	EVENT_CAMERA_DOCK,
+	EVENT_CAMERA_WINDOW_POS,
+	EVENT_CAMERA_WINDOW_SIZE
+};
+
+static enum DATA_REQUEST_ID
+{
+	REQUEST_ID_0
+};
+
+HRESULT CreateCamera()
+{
+	HRESULT hr;
+
+	SIMCONNECT_DATA_XYZ position;
+	position.x = 0;
+	position.y = 0;
+	position.z = 0;
+
+	SIMCONNECT_DATA_PBH rotation;
+	rotation.Pitch = 0;
+	rotation.Bank = 0;
+	rotation.Heading = 0;
+
+	GUID guid;
+	CoCreateGuid(&guid);
+	SIMCONNECT_CAMERA_TYPE cameraType = SIMCONNECT_CAMERA_TYPE_OBJECT_CENTER;
+
+	hr = SimConnect_CreateCameraDefinition(hSimConnect, guid, cameraType, CAMERA_NAME, position, rotation);
+	if (hr == S_OK)
+	{
+		hr = SimConnect_CreateCameraInstance(hSimConnect, guid, CAMERA_NAME, SIMCONNECT_OBJECT_ID_USER, REQUEST_ID_0);
+		if (hr == S_OK)
+		{
+			hr = SimConnect_OpenView(hSimConnect, CAMERA_NAME);
+			if (hr == S_OK)
+			{
+				CAMERA_CREATED = true;
+			}
+		}
+	}
+	return hr;
+}
+
+// Used to cycle between two different positions on the screen
+void GetWindowPos(int& x, int&y)
+{
+	if (WINDOW_POS_TOGGLE)
+	{
+		x = X1;
+		y = Y1;
+	}
+	else
+	{
+		x = X2;
+		y = Y2;
+	}
+	WINDOW_POS_TOGGLE = !WINDOW_POS_TOGGLE;
+}
+
+// Used to cycle the width and height between two different values
+void GetWindowSize(int& width, int& height)
+{
+	if (WINDOW_SIZE_TOGGLE)
+	{
+		width = W1;
+		height = H1;
+	}
+	else
+	{
+		width = W2;
+		height = H2;
+	}
+	WINDOW_SIZE_TOGGLE = !WINDOW_SIZE_TOGGLE;
+}
+
+void CALLBACK MyDispatchProcCC(SIMCONNECT_RECV* pData, DWORD cbData, void *pContext)
+{
+	if (!pData)
+	{
+		printf("SimConnect callback error.");
+		return;
+	}
+
+	HRESULT hr;
+
+	switch (pData->dwID)
+	{
+	case SIMCONNECT_RECV_ID_EVENT:
+	{
+		SIMCONNECT_RECV_EVENT *evt = (SIMCONNECT_RECV_EVENT*)pData;
+
+		switch (evt->uEventID)
+		{
+		case EVENT_CAMERA_CREATE:
+
+			if (!CAMERA_CREATED)
+			{
+				hr = CreateCamera();
+				if (hr == S_OK)
+				{
+					printf("\nCamera Created");
+				}
+				else
+				{
+					printf("\nSimConnect error - Create Camera");
+				}
+			}
+
+			break;
+
+		case EVENT_CAMERA_UNDOCK:
+
+			if (CAMERA_CREATED)
+			{
+				hr = SimConnect_UndockView(hSimConnect, CAMERA_NAME);
+				if (hr == S_OK)
+				{
+					printf("\nView Undocked");
+				}
+				else
+				{
+					printf("\nSimConnect error - Undock Camera");
+				}
+			}
+			else
+			{
+				printf("\nCreate camera first! (press 'E')");
+			}
+
+			break;
+
+		case EVENT_CAMERA_DOCK:
+
+			if (CAMERA_CREATED)
+			{
+				hr = SimConnect_DockView(hSimConnect, CAMERA_NAME);
+				if (hr == S_OK)
+				{
+					printf("\nView Docked");
+				}
+				else
+				{
+					printf("\nSimConnect error - Dock Camera");
+				}
+			}
+			else
+			{
+				printf("\nCreate camera first! (press 'E')");
+			}
+
+			break;
+
+		case EVENT_CAMERA_WINDOW_POS:
+
+			if (CAMERA_CREATED)
+			{
+				int x;
+				int y;
+				GetWindowPos(x, y);
+				hr = SimConnect_SetCameraWindowPosition(hSimConnect, CAMERA_NAME, x, y);
+				if (hr == S_OK)
+				{
+					printf("\nWindow position updated");
+				}
+				else
+				{
+					printf("\nSimConnect error - Window Position");
+				}
+			}
+			else
+			{
+				printf("\nCreate camera first! (press 'E')");
+			}
+
+			break;
+
+		case EVENT_CAMERA_WINDOW_SIZE:
+
+			if (CAMERA_CREATED)
+			{
+				int width;
+				int height;
+				GetWindowSize(width, height);
+				hr = SimConnect_SetCameraWindowSize(hSimConnect, CAMERA_NAME, width, height);
+				if (hr == S_OK)
+				{
+					printf("\nWindow size updated");
+				}
+				else
+				{
+					printf("\nSimConnect error - Window Size");
+				}
+			}
+			else
+			{
+				printf("\nCreate camera first! (press 'E')");
+			}
+
+			break;
+
+		default:
+
+			break;
+		}
+
+		break;
+	}
+
+	case SIMCONNECT_RECV_ID_EXCEPTION:
+	{
+		SIMCONNECT_RECV_EXCEPTION *except = (SIMCONNECT_RECV_EXCEPTION*)pData;
+
+		switch (except->dwException)
+		{
+		case SIMCONNECT_EXCEPTION_ERROR:
+
+			printf("\nCamera error");
+			break;
+
+		default:
+
+			printf("\nException");
+			break;
+		}
+		break;
+	}
+
+	case SIMCONNECT_RECV_ID_QUIT:
+	{
+		G_QUIT = true;
+		break;
+	}
+
+	default:
+		break;
+	}
+}
+
+void TestCameraSystem()
+{
+	HRESULT hr;
+
+	if (SUCCEEDED(SimConnect_Open(&hSimConnect, PROJECT_NAME, NULL, 0, 0, 0)))
+	{
+		printf("\nConnected to Prepar3D!");
+
+		// Define private events
+		hr = SimConnect_MapClientEventToSimEvent(hSimConnect, EVENT_CAMERA_CREATE);
+		hr = SimConnect_MapClientEventToSimEvent(hSimConnect, EVENT_CAMERA_UNDOCK);
+		hr = SimConnect_MapClientEventToSimEvent(hSimConnect, EVENT_CAMERA_DOCK);
+		hr = SimConnect_MapClientEventToSimEvent(hSimConnect, EVENT_CAMERA_WINDOW_POS);
+		hr = SimConnect_MapClientEventToSimEvent(hSimConnect, EVENT_CAMERA_WINDOW_SIZE);
+
+		// Group inputs
+		hr = SimConnect_AddClientEventToNotificationGroup(hSimConnect, GROUP0, EVENT_CAMERA_CREATE);
+		hr = SimConnect_AddClientEventToNotificationGroup(hSimConnect, GROUP0, EVENT_CAMERA_UNDOCK);
+		hr = SimConnect_AddClientEventToNotificationGroup(hSimConnect, GROUP0, EVENT_CAMERA_DOCK);
+		hr = SimConnect_AddClientEventToNotificationGroup(hSimConnect, GROUP0, EVENT_CAMERA_WINDOW_POS);
+		hr = SimConnect_AddClientEventToNotificationGroup(hSimConnect, GROUP0, EVENT_CAMERA_WINDOW_SIZE);
+
+		// Set priorities
+		hr = SimConnect_SetNotificationGroupPriority(hSimConnect, GROUP0, SIMCONNECT_GROUP_PRIORITY_HIGHEST);
+
+		// Map the keys , and . keys to the private events
+		hr = SimConnect_MapInputEventToClientEvent(hSimConnect, GROUP0, "E", EVENT_CAMERA_CREATE);
+		hr = SimConnect_MapInputEventToClientEvent(hSimConnect, GROUP0, "J", EVENT_CAMERA_UNDOCK);
+		hr = SimConnect_MapInputEventToClientEvent(hSimConnect, GROUP0, "K", EVENT_CAMERA_DOCK);
+		hr = SimConnect_MapInputEventToClientEvent(hSimConnect, GROUP0, "VK_COMMA", EVENT_CAMERA_WINDOW_POS);
+		hr = SimConnect_MapInputEventToClientEvent(hSimConnect, GROUP0, "VK_PERIOD", EVENT_CAMERA_WINDOW_SIZE);
+
+		// Set group 0 to be active on startup
+		hr = SimConnect_SetInputGroupState(hSimConnect, GROUP0, SIMCONNECT_STATE_ON);
+
+		// Show controls to user
+		printf(CONTROLS);
+
+		while (!G_QUIT)
+		{
+			SimConnect_CallDispatch(hSimConnect, MyDispatchProcCC, NULL);
+			Sleep(SIMCONNECT_SLEEP_TIME);
+		}
+
+		hr = SimConnect_Close(hSimConnect);
+	}
+	else
+	{
+		printf("\nFailed to Connect");
+	}
+}
+
+int __cdecl _tmain(int argc, _TCHAR* argv[])
+{
+	TestCameraSystem();
+
+	return 0;
+}
